915d6aa82203bcb37f036c6985d4ed6b0afff434
AnanthuManoj <120579729+AnanthuManoj@users.noreply.github.com>
2024-07-12 14:42:15 +0530

    changed the shape of images

--- client/components/About/AboutSectionTwo.tsx
+++ client/components/About/AboutSectionTwo.tsx
@@ -5,7 +5,7 @@ const AboutSectionTwo = () => {
     <section className="py-16 md:py-20 lg:py-12">
       <div className="container">
         <div className="-mx-auto flex flex-wrap items-center">
-          <div className=" w-full px-4 lg:w-1/2 order-2 md:order-1">
+          <div className="w-full px-4 lg:w-1/2 order-2 md:order-1">
             <div
               className="flex items-center relative aspect-[1/1] max-w-[500px] lg:mr-0"
               data-wow-delay=".2s"
@@ -16,8 +16,8 @@ const AboutSectionTwo = () => {
                 objectFit="cover"
                 width={70}
                 height={70}
-                className="rounded-lg absolute mt-96 md:ml-20 lg:ml-0 md:z-10 "
-              // className="mx-auto max-w-full lg:mr-0"
+                className="absolute mt-96 md:ml-20 lg:ml-0 md:z-10"
+                style={{ clipPath: "polygon(25% 0%, 75% 0%, 100% 50%, 75% 100%, 25% 100%, 0% 50%)" }} // Hexagon
               />
               <Image
                 src="/images/about/about-image.jpg"
@@ -25,8 +25,8 @@ const AboutSectionTwo = () => {
                 objectFit="cover"
                 width={350}
                 height={350}
-                className="rounded-lg absolute mb-32 md:mb-24 md:mt-96 md:ml-40 lg:ml-16 border-white border-spacing-10 border"
-              // className="mx-auto max-w-full lg:mr-0"
+                className="absolute mb-32 md:mb-24 md:mt-96 md:ml-40 lg:ml-16 border-white border-spacing-10 border"
+                style={{ clipPath: "polygon(25% 0%, 75% 0%, 100% 50%, 75% 100%, 25% 100%, 0% 50%)" }} // Hexagon
               />
               <Image
                 src="/images/about/about-image-2.jpg"
@@ -34,8 +34,8 @@ const AboutSectionTwo = () => {
                 objectFit="cover"
                 width={350}
                 height={350}
-                className="rounded-lg absolute md:ml-72 lg:ml-28 xl:ml-52 border-white border-spacing-3 border hidden md:block"
-              // className="mx-auto max-w-full lg:mr-0"
+                className="absolute md:ml-72 lg:ml-28 xl:ml-52 border-white border-spacing-3 border hidden md:block"
+                style={{ clipPath: "polygon(25% 0%, 75% 0%, 100% 50%, 75% 100%, 25% 100%, 0% 50%)" }} // Hexagon
               />
               <Image
                 src="/images/gallery/AJI_3786.jpg"
@@ -43,27 +43,25 @@ const AboutSectionTwo = () => {
                 objectFit="cover"
                 width={350}
                 height={350}
-                className="rounded-lg absolute md:mb-80 lg:mb-96 border-white border-spacing-10 border hidden md:block"
-              // className="mx-auto max-w-full lg:mr-0"
+                className="absolute md:mb-80 lg:mb-96 border-white border-spacing-10 border hidden md:block"
+                style={{ clipPath: "polygon(25% 0%, 75% 0%, 100% 50%, 75% 100%, 25% 100%, 0% 50%)" }} // Hexagon
               />
               <div className="flex flex-row mt-56 md:hidden">
                 <Image
                   src="/images/about/about-image-2.jpg"
                   alt="about-image"
-                  // objectFit="cover"
                   width={50}
                   height={50}
-                  className="rounded-lg border-white border-spacing-3 border w-[50vw] "
-                // className="mx-auto max-w-full lg:mr-0"
+                  className="border-white border-spacing-3 border w-[50vw]"
+                  style={{ clipPath: "polygon(25% 0%, 75% 0%, 100% 50%, 75% 100%, 25% 100%, 0% 50%)" }} // Hexagon
                 />
                 <Image
                   src="/images/gallery/AJI_3786.jpg"
                   alt="about-image"
-                  // objectFit="cover"
                   width={50}
                   height={50}
-                  className="rounded-lg border-white border-spacing-10 border w-[50vw]"
-                // className="mx-auto max-w-full lg:mr-0"
+                  className="border-white border-spacing-10 border w-[50vw]"
+                  style={{ clipPath: "polygon(25% 0%, 75% 0%, 100% 50%, 75% 100%, 25% 100%, 0% 50%)" }} // Hexagon
                 />
               </div>
             </div>
